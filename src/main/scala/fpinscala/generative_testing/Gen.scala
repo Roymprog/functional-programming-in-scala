b8fcbcd2f231b1e22b7d9cc57e4d24df8eb3f694
Roy van Santen <royvsanten@gmail.com>
2020-09-20 16:36:35 +0200

    Add solutions until 8.5

---
+++ src/main/scala/fpinscala/generative_testing/Gen.scala
@@ -0,0 +1,53 @@
+package fpinscala.generative_testing
+
+import fpinscala.functional_state.RNG.nonNegativeInt
+import fpinscala.functional_state.State.sequence
+import fpinscala.functional_state.{RNG, SimpleRNG, State}
+import fpinscala.generative_testing.Prop.{FailedCase, SuccessCount}
+
+object Gen {
+  // 8.5 Define functions unit, boolean and listOfN
+  def unit[A](a: => A): Gen[A] = Gen(State((rng: RNG) => (a, rng)))
+
+  def boolean: Gen[Boolean] = Gen(State(RNG.map((rng: RNG) => nonNegativeInt(rng))(i => i % 2 == 1)))
+
+  def listOf[A](a: Gen[A]): Gen[List[A]] = ???
+
+  def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]] = {
+    val list: List[State.State[RNG, A]] =
+      (0 to n).map(_ => (rng: RNG) => a.sample.run(rng)).toList
+    val randlist: State.State[RNG, List[A]] = sequence(list)
+    Gen(State((rng: RNG) => randlist(rng)))
+  }
+
+  def forAll[A](gen: Gen[A])(pred: A => Boolean): Prop = ???
+
+  // 8.4 Define function Gen.choose that generates numbers between start and stopExclusive
+  // Assuming start >= 0 and stopExclusive > start
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    val state = State(RNG.map((rng: RNG) => nonNegativeInt(rng))(i => i % (stopExclusive - start) + start))
+    Gen(state)
+  }
+}
+
+case class Gen[A](sample: State[RNG, A])
+
+trait Prop {
+  def check: Either[(FailedCase, SuccessCount),SuccessCount]
+}
+
+trait PropBool {
+  def check: Boolean
+  // 8.3 Implement Prop as a Boolean only signalling success or failure
+  def &&(p: PropBool): PropBool = {
+    val thisProp = this
+    new PropBool {
+      override def check: Boolean = thisProp.check && p.check
+    }
+  }
+}
+
+object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
+}
